2e07f220bfc8d7874bff3a4d4d4a9adaa1a219b2
Decrypt <louis.vicomte@protonmail.com>
2026-05-09 10:11:31 +0200

    Update oak-sync to v1.0.2

--- Formula/oak-sync.rb
+++ Formula/oak-sync.rb
@@ -1,8 +1,8 @@
 class OakSync < Formula
   desc "CLI tool for syncing Oak Research production branches with preprod"
   homepage "https://github.com/Decryptu/oak-sync"
-  url "https://github.com/Decryptu/oak-sync/archive/refs/tags/v1.0.1.tar.gz"
-  sha256 "8e4c2bfa128480ab31da9d2ff7226a029118b4882ebe956b3b29762157c48c3c"
+  url "https://github.com/Decryptu/oak-sync/archive/refs/tags/v1.0.2.tar.gz"
+  sha256 "024fee5b0de7a178cb66f9e5009e3fe0b48b5e3f998796779b50358e04ee6797"
   license "MIT"
 
   depends_on "gum"
